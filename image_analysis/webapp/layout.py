7b85b2f69412db3e1654d4dbbfa79132c5489576
Ebad Kamil <ebad.kamil@xfel.eu>
2019-09-14 16:47:10 +0200

    Add get_layout in layout

--- image_analysis/webapp/layout.py
+++ image_analysis/webapp/layout.py
@@ -197,3 +197,80 @@ def get_plot_tab(config):
             className="row"),
         ])
     return div
+
+
+def get_layout(UPDATE_INT, config=None):
+
+    app_layout = html.Div([
+
+        html.Div([
+            dcc.Interval(
+                id='interval_component',
+                interval=UPDATE_INT * 1000,
+                n_intervals=0),
+            dcc.Interval(
+                id='psutil_component',
+                interval=2 * 1000,
+                n_intervals=0)], style=dict(textAlign='center')),
+
+        html.Div([
+            daq.Gauge(
+                id='virtual_memory',
+                min=0,
+                value=0,
+                size=150,
+                className="leftbox",
+                style=dict(textAlign="center")),
+            daq.Gauge(
+                id='swap_memory',
+                min=0,
+                value=0,
+                size=150,
+                className="rightbox",
+                style=dict(textAlign="center")
+            ),
+        ]),
+        daq.LEDDisplay(
+            id='train-id',
+            value=1000,
+            color="#FF5E5E",
+            style=dict(textAlign="center")),
+
+        html.Br(),
+
+        html.Div(children=[
+            dcc.Tabs(
+                parent_className='custom-tabs',
+                className='custom-tabs-container',
+                id='view-tabs',
+                value='plot',
+                children=[
+                    dcc.Tab(
+                        className="custom-tab",
+                        selected_className='custom-tab--selected',
+                        label='Stream/Load data',
+                        value='stream-data',
+                        children=get_stream_tab(config)
+                    ),
+
+                    # dcc.Tab(
+                    #     className="custom-tab",
+                    #     selected_className='custom-tab--selected',
+                    #     label='Experimental parameters',
+                    #     value='exp-param',
+                    #     children=get_exp_tab()
+                    # ),
+
+                    dcc.Tab(
+                        className="custom-tab",
+                        selected_className='custom-tab--selected',
+                        label='Plots',
+                        value='plot',
+                        children=get_plot_tab(config)
+                    )
+                ])
+        ])
+
+    ])
+
+    return app_layout
