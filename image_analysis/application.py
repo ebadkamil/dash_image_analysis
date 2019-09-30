a4918df8e640fa29ce6a07e922fae7a76220a611
Ebad Kamil <ebad.kamil@xfel.eu>
2019-09-30 21:47:53 +0200

    Add application to run from command line

---
+++ image_analysis/application.py
@@ -0,0 +1,34 @@
+"""
+Image analysis and web visualization
+
+Author: Ebad Kamil <kamilebad@gmail.com>
+Copyright (C) European X-Ray Free-Electron Laser Facility GmbH.
+All rights reserved.
+"""
+import argparse
+
+from .webapp import DashApp
+
+
+def run_dashservice():
+    ap = argparse.ArgumentParser(prog="webImageAnalyis")
+    ap.add_argument("detector", help="detector name (case insensitive)",
+                    choices=[det.upper()
+                             for det in ["jungfrau", "LPD", "AGIPD"]],
+                    type=lambda s: s.upper())
+    ap.add_argument("hostname", help="Hostname")
+    ap.add_argument("port", help="TCP port to run server on")
+    args = ap.parse_args()
+
+    detector = args.detector
+    if detector == 'JUNGFRAU':
+        detector = 'JungFrau'
+
+    hostname = args.hostname
+    port = args.port
+
+    app = DashApp(detector, hostname, port)
+    app.recieve()
+    app.process()
+
+    app._app.run_server(debug=False)
